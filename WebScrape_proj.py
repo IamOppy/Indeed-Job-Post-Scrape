09d9c210d2cce838183ca49db22a1374d19d9c14
IamOppy <randolffretrato@yahoo.com>
2021-11-16 00:18:23 -0800

    'Updated'

--- WebScrape_proj.py
+++ WebScrape_proj.py
@@ -6,12 +6,9 @@ import requests #REQUEST PAGE
 parser = argparse.ArgumentParser()
 parser.add_argument('--get', type=str)
 args = parser.parse_args()
-r = requests.get('http://toscrape.com/')
+r = requests.get('https://ca.indeed.com/jobs?q=python%20programmer&l=Toronto%2C%20ON')
 soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.prettify())
 
-def GET_TAGS():
-	print('getting')
 
 
 
@@ -22,8 +19,20 @@ def SAVE_TAGS():
 def VIEW_SAVED_TAGS():
 	pass
 
+
+def GET_TAGS_LINKS():
+	Job_Titles = [Job_name for Job_name in soup.find_all('span')]
+	Company_name = []
+	for tag in lst:
+		tags = tag.get('title')
+		if tags != None:
+			print(tags)
+
 def main():
 	if args.get == 'tags':
-		GET_TAGS()
+		GET_TAGS_LINKS()
 	if args.get == 'view':
 		VIEW_SAVED_TAGS()
+
+if __name__ == "__main__":
+	main()
